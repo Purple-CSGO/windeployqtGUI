d2ac449565257dd9cc70295e45e9e375677d4664
Purple-CSGO <438518244@qq.com>
2020-01-17 20:07:10 +0800

    Basic ui design

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 
 #include <QApplication>
 
+
 int main(int argc, char *argv[])
 {
+    QString version = "V0.1";   //版本号
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("windeployqtGUI - " + version);
     w.show();
     return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,8 @@
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
